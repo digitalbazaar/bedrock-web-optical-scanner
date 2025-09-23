fbe5e8338234507809e0af486fe13042e06381a0
Mandy Venables <mandylvenables@gmail.com>
2025-09-23 16:47:30 -0400

    Move index.js into /lib.

--- index.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) 2025 Digital Bazaar, Inc. All rights reserved.
- */
-
-export {OpticalScanner} from './lib/optical-scanner.js';
-export * from './lib/plugins/index.js';
-export * as cameraUtils from './lib/utils/camera.js';
