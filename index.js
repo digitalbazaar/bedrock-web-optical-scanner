52858d48fc0fa68071df10d1a9d0e82d242d860d
Parth Bhatt <pbjm2493@gmail.com>
2025-09-25 17:12:55 -0700

    Export CameraScanner class

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * Copyright (c) 2025 Digital Bazaar, Inc. All rights reserved.
  */
-
+export {CameraScanner} from './lib/camera-scanner.js';
 export {OpticalScanner} from './lib/optical-scanner.js';
 export * from './lib/plugins/index.js';
 export * as cameraUtils from './lib/utils/camera.js';
