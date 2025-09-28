fabc26e499c0b3fa880ca04395c8baf9c9df135f
Parth Bhatt <pbjm2493@gmail.com>
2025-09-28 12:14:11 -0700

    Initial setup and cleanup for new library

--- test/test.config.js
+++ test/test.config.js
@@ -3,10 +3,10 @@
  */
 import {config} from '@bedrock/core';
 import {createRequire} from 'node:module';
-import path from 'node:path';
+// import path from 'node:path';
 const require = createRequire(import.meta.url);
 
-config.karma.suites['bedrock-web-pouch-edv'] = path.join('web', '**', '*.js');
+// config.karma.suites['bedrock-web-pouch-edv'] = path.join('web', '**', '*.js');
 
 config.karma.config.webpack.resolve.fallback.events =
   require.resolve('events/');
